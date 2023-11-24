14979573c30d710340c13a4fd5cdd7c500d7a906
Paulo Ortolan <paulo.ortolan@gmail.com>
2023-11-24 14:29:06 +0000

    Adding real source code

---
+++ src/database/database_connection_manager.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+class DatabaseConnectionManager:
+    _connection = None
+    _datasource = "./../attachments.sqlite3"
+
+    def __init__(self):
+        self._connect()
+
+    def _connect(self):
+        self._connection = sqlite3.connect(self._datasource, timeout=10)
+
+    def get_connection(self):
+        return self._connection
+
+    def reconnect(self):
+        self._connect()
---
+++ src/gmail/gmail_service.py
@@ -0,0 +1,51 @@
+"""
+Gmail Service
+"""
+import os
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+class GmailService:
+    """
+    Class Gmail Service
+    """
+    _SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+    _service = None
+
+    _secrets_dir = "./secrets/"
+
+    def __init__(self):
+        """
+        Creates a service for gmail.
+        """
+        token_path = f"{self._secrets_dir}token.json"
+        credentials_path = f"{self._secrets_dir}credentials.json"
+
+        creds = None
+        if os.path.exists(token_path):
+            creds = Credentials.from_authorized_user_file(token_path, self._SCOPES)
+
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    credentials_path, self._SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+
+            with open(token_path, "w") as token:
+                token.write(creds.to_json())
+
+        self._service = build("gmail", "v1", credentials=creds)
+
+    def service(self):
+        """
+        Gets the cretated service
+        :return: the created service
+        """
+        return self._service
